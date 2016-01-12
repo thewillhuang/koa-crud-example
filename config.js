71871345afb6261f198ee6bfec3fcc046b306bfe
William <will.h86@gmail.com>
2016-01-11 21:30:07 -0800

    try defaults

--- config.js
+++ config.js
@@ -1,15 +1,21 @@
-const port = process.env.PORT || 3001;
-// 'postgres://username:password@localhost:port/database';
-const connectionString = process.env.PGCONNECTIONSTRING
-  || 'postgres://willh@localhost/willh';
-const env = process.env.NODE_ENV || 'development';
+const {
+  PORT = 3001,
+  PGCONNECTIONSTRING = 'postgres://willh@localhost/willh',
+  NODE_ENV = 'development',
+  DATABASE = 'wrenchsessions',
+  HOST = 'localhost',
+  MYSQLUSER = 'root',
+  PASSWORD = '',
+} = process.env;
+const port = PORT;
+const connectionString = PGCONNECTIONSTRING;
+const env = NODE_ENV;
 const mysqldb = {
-  database: process.env.DATABASE || 'wrenchsessions',
+  database: DATABASE,
   connectionLimit: 100,
-  // aws rds
-  host: process.env.HOST || 'localhost',
-  user: process.env.USER || 'root',
-  // password: process.env.PASSWORD || '',
+  host: HOST,
+  user: MYSQLUSER,
+  password: PASSWORD,
   // ssl: 'Amazon RDS',
 };
 
